afa98f84e1afb003fd3c74327928644c1423f593
dev-Lokesh-dev <Lokeshsharma7778@gmail.com>
2025-01-19 17:50:15 +0530

    api delpyed

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/users",userRouter)
 
-app.listen(9000,async()=>{
+app.listen(9879,async()=>{
     await mongoose.connect("mongodb+srv://lokeshsharma7779:biKav0aC1QLSwQ4F@cluster0.ir5h7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Database connected");
     console.log("server started at http://localhost:9000")
